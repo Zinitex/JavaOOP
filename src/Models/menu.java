bcdc4ef7918ca6935b778c90ba8330c0bda8aee6
Zinitex <Nanda30rizky@gmail.com>
2024-12-21 13:16:32 +0700

    feat: add menu and pemesanan model classes

---
+++ src/Models/menu.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+/**
+ *
+ * @author nanda
+ */
+public class menu {
+
+    public menu(String nama, String desc, String harga) {
+        this.nama = nama;
+        this.desc = desc;
+        this.harga = harga;
+    }
+    public String nama;
+    public String desc;
+    public String harga;
+    
+    
+}
---
+++ src/Models/pemesanan.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+import java.util.List;
+
+/**
+ *
+ * @author nanda
+ */
+public class pemesanan {
+
+    public String email;
+    public String tanggal;
+    public String total_harga;
+    private List<menu> Menu;
+}
